559bd901c0d07934b6e7e535d188ed6ad8853d8d
slv-improver <k.moret.slv@gmail.com>
2022-09-13 12:42:33 +0100

    add another pages

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Liste d\'annonces</h1><p>Nothing here</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Contactez-nous !</h1><p>Formulaire</p>')
