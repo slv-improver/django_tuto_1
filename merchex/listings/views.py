1b6cf4f4febacbca91ccdc055ea7f21555835844
slv-improver <k.moret.slv@gmail.com>
2022-09-13 18:55:23 +0100

    create Listing object & display them in listings.views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band
+from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
+    listings = Listing.objects.all()
     return HttpResponse(f"""
         <h1>Hello Django!</h1>
+        <p>Les annonces</p>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+            <li>{listings[3].title}</li>
+        </ul>
         <p>Mes plats préférés sont :<p>
         <ul>
             <li>{bands[0].name}</li>
