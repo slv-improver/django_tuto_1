31075129d14c13e6351e89249795480974957e9d
slv-improver <k.moret.slv@gmail.com>
2022-09-15 09:31:45 +0100

    add templates directory and export hmtl block from views.hello() to hello.html

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,23 +5,11 @@ from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
-    listings = Listing.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django!</h1>
-        <p>Les annonces</p>
-        <ul>
-            <li>{listings[0].title}</li>
-            <li>{listings[1].title}</li>
-            <li>{listings[2].title}</li>
-            <li>{listings[3].title}</li>
-        </ul>
-        <p>Mes plats préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-    """)
+    return render(
+        request,
+        'listings/hello.html',
+        {'bands': bands}
+    )
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
